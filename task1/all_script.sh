fafe83ce59db214c9ef173f27de7e54489c0dd56
Hegel <sobotafran@gmail.com>
2026-01-13 15:51:58 +0100

    add rm

--- task1/all_script.sh
+++ task1/all_script.sh
@@ -2,12 +2,13 @@
 
 set +ex
 
-# rm -r data  
+rm -r data  
 ./generate_script.sh
 
 rm -r data/*/trajectories/
 ./simulate_script.sh
 
+rm all_data.csv
 rm data/bn*/reconstruction_results.csv
 ./infer_script.sh
 
